812a772da1aa5d95600312248e9011230fe4cb6d
kim <332593916@qq.com>
2024-05-02 15:54:06 +0800

    添加选项页

---
+++ english/src/options/options.js
@@ -0,0 +1,5 @@
+import Vue, { h, render } from 'vue'
+import Options from '@/components/Options.vue'
+new Vue({
+    render: h => h(Options)
+}).$mount('#app')
--- english/vue.config.js
+++ english/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    options: {
+      entry: 'src/options/main.js', // 指定选项页面的入口文件
+      template: 'public/options.html', // 指定选项页面的 HTML 模板文件
+      filename: 'options.html' // 指定生成的 HTML 文件名
+    }
+  }
 })
